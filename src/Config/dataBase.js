6e691fb090d3e0e48c9ca53e6ec6314e39680f96
shradhanand patil <shradhanandpatil200411>
2025-04-01 23:21:40 +0530

    add dataBase and create signUp post api

---
+++ src/Config/dataBase.js
@@ -0,0 +1,12 @@
+// mongodb+srv://shradhanand:BZX8E6V7566UUUHu@studymongodb.fovl7.mongodb.net/DevTinder
+
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  console.log("DataBase is Connect");
+  await mongoose.connect(
+    "mongodb+srv://shradhanand:BZX8E6V7566UUUHu@studymongodb.fovl7.mongodb.net/DevTinder"
+  );
+};
+
+module.exports = connectDB;
--- src/app.js
+++ src/app.js
@@ -1,11 +1,30 @@
 const express = require("express");
-
+const connectDB = require("./Config/dataBase");
 const app = express();
+const User = require("./Model/userSchema");
 
-app.use((req, res) => {
-  res.send("Hello world");
+app.post("/signup", async (req, res) => {
+  const user = new User({
+    firstName: "Shradhanand",
+    lastName: "Patil",
+    email: "Shradhanand@patil.gmail.com",
+    password: "Shradhanand@123",
+  });
+
+  try {
+    await user.save();
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("user is not save :" + err.message);
+  }
 });
 
-app.listen(3000, () => {
-  console.log("Hello vicky patil ");
-});
+connectDB()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server Start Vicky");
+    });
+  })
+  .catch(() => {
+    console.error("Server is not responding");
+  });
