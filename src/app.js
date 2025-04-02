49b55ee8a2d3d5e77de05793a5cf53fe4522a057
shradhanand patil <shradhanandpatil200411>
2025-04-02 23:31:22 +0530

    create a get api

--- src/app.js
+++ src/app.js
@@ -3,19 +3,55 @@ const connectDB = require("./Config/dataBase");
 const app = express();
 const User = require("./Model/userSchema");
 
+app.use(express.json());
+
 app.post("/signup", async (req, res) => {
-  const user = new User({
-    firstName: "Shradhanand",
-    lastName: "Patil",
-    email: "Shradhanand@patil.gmail.com",
-    password: "Shradhanand@123",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
     res.send(user);
   } catch (err) {
     res.status(400).send("user is not save :" + err.message);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const getAllUser = await User.find({});
+    if (getAllUser.length === 0) {
+      res.status(404).send("Data Not Found");
+    } else {
+      res.send(getAllUser);
+    }
+  } catch (err) {
+    res.status(404).send("Data Not Found");
+  }
+});
+
+app.get("/findByEmail", async (req, res) => {
+  try {
+    const getData = await User.findOne({ email: req.body.email });
+    if (!getData) {
+      res.status(404).send("Data Not Found");
+    } else {
+      res.send(getData);
+    }
+  } catch (err) {
+    res.status(404).send("Data Not Found");
+  }
+});
+
+app.get("/findById", async (req, res) => {
+  try {
+    const getData = await User.findById({ _id: req.body._id });
+    if (!getData) {
+      res.status(404).send("Data Not Found 1");
+    } else {
+      res.send(getData);
+    }
+  } catch (err) {
+    res.status(404).send("Data Not Found");
   }
 });
 
