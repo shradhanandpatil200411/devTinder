0b3c3ac37c7d8741b229014d40d68db9b434f5dd
shradhanand patil <shradhanandpatil200411>
2025-04-03 16:37:18 +0530

    add validation on schema

--- src/Model/userSchema.js
+++ src/Model/userSchema.js
@@ -1,18 +1,40 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      require: true,
+      trim: true,
+    },
+    lastName: {
+      type: String,
+      trim: true,
+    },
+    email: {
+      type: String,
+      require: true,
+      trim: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      trim: true,
+    },
+    photoUrl: {
+      type: String,
+      default:
+        "https://imgs.search.brave.com/WN4fV9d_eOtAjhegA4Zm8aDc477mMeIKoY7NtEBEuNo/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vcGhvdG9z/L2ljb24tb2YtYS1i/dXNpbmVzc21hbi1h/dmF0YXItb3ItcHJv/ZmlsZS1waWMtcGlj/dHVyZS1pZDQ3NDAw/MTg5Mj9rPTIwJm09/NDc0MDAxODkyJnM9/NjEyeDYxMiZ3PTAm/aD1xbVAya2NWbENn/QVhvVVVyalROb0ln/Vm9qLVQ0d1RyTVhn/YVUwTkV5NFJnPQ",
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("User", userSchema);
