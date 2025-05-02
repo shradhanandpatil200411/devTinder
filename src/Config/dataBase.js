f663129310ca5ed01b10b5352cdf2eb860a91fcc
shradhanand patil <shradhanandpatil200411>
2025-05-02 22:52:09 +0530

    change password db

--- src/Config/dataBase.js
+++ src/Config/dataBase.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   console.log("DataBase is Connect");
   await mongoose.connect(
-    "mongodb+srv://shradhanand:BZX8E6V7566UUUHu@studymongodb.fovl7.mongodb.net/DevTinder"
+    "mongodb+srv://shradhanand:6wy067GnZH2KnGbF@studymongodb.fovl7.mongodb.net/DevTinder"
   );
 };
 
