fa110a1b88da785c83aac650604f4d850e17e607
shradhanand patil <shradhanandpatil200411>
2025-05-04 00:00:15 +0530

    port change

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "http://localhost:5173/",
     credentials: true,
   })
 );
@@ -25,8 +25,8 @@ app.use("/", userRouter);
 
 connectDB()
   .then(() => {
-    app.listen(3000, () => {
-      console.log("Server Start Vicky");
+    app.listen(3000, "0.0.0.0", () => {
+      console.log("Server Start on port 3000");
     });
   })
   .catch(() => {
